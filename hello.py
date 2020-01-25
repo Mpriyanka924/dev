c7a695da4cc023a363e4c5671d9b68c1888ffcff
Mpriyanka924 <priyankam1102@gmail.com>
2020-01-25 06:59:08 +0530

    hello

--- hello.py
+++ hello.py
@@ -1,21 +1,11 @@
-{
-    "projects": {
-      "project": [
-        {
-          "name": "Unisys",
-          "solution": "PackingStation\\BLogic.Applications.PackingStation.sln",
-          "analysisFiles": "BLogic.Applications.PackingStation.exe"
-        },
-        {
-          "name": "ABC",
-          "solution": "MasterData\\BLogic.Applications.MasterData.sln",
-          "analysisFiles": "BLogic.Applications.MasterData.exe"
-        },
-        {
-          "name": "XYZ",
-          "solution": "OrderManager\\BLogic.Applications.OrderManager.sln",
-          "analysisFiles": "BLogic.Applications.OrderManager.exe"
-        }
-      ]
-    }
-  }
+#! /usr/bin/python3
+print("Hello, Unisys")
+
+def pig_latin(word):
+    if word[0] in 'aeiou' or 'AEIOU':
+        pig_word = word + 'ay'
+    else:
+        pig_word = word[1:] + word[0] + 'ay'
+    return pig_word
+
+print(pig_latin('TAJ'))
